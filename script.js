7f13ba26697c626e59bb8a3776a16e83143ac338
bodie101 <keysbw@g.toiohomai.ac.nz>
2017-09-04 11:39:21 +1200

    inital commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+alert("This program will tell you what number is greater");
+
+let num1 = prompt("Please enter a number", "00"); 
+ num1 = parseint(input1);
+
+let num2 = prompt("Please enter another number", "00"); 
+ num2 = Parseint(input2);
+
+if (input1 < input2)
+    {
+        alert("number2 is bigger than number1")
+    }
+if (input2 < input1)
+    {
+        alert("number1 is bigger than number2")
+    }
+if (input1 == input2)
+    {
+        alert("They are equal")
+    }
+
+    console.log(txt);
+
+    
+
+
+
+
